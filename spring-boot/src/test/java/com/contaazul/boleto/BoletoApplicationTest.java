2b04c3980ff8ae595a4bf699d553de9882ae0ecb
Wellton S. Barros <makotostudiodev@gmail.com>
2018-11-05 18:14:44 -0200

    Configurações Iniciais

---
+++ spring-boot/src/test/java/com/contaazul/boleto/BoletoApplicationTest.java
@@ -0,0 +1,17 @@
+package com.contaazul.boleto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoletoApplicationTest {
+
+    @Test
+    public void loadContext() {
+
+    }
+
+}
