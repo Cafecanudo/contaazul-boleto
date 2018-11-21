8cf05d554cd6791b952365ee8b9ad52da32f4a4f
Cafecanudo <39340795+Cafecanudo@users.noreply.github.com>
2018-11-21 09:50:26 -0200

    Update ApiErrorResponse.java
    Corrigindo Lombok para sonar nao mostrar Codesmell

--- src/main/java/com/contaazul/boleto/exceptions/ApiErrorResponse.java
+++ src/main/java/com/contaazul/boleto/exceptions/ApiErrorResponse.java
@@ -1,11 +1,13 @@
 package com.contaazul.boleto.exceptions;
 
+import lombok.Generated;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
+@Generated
 public class ApiErrorResponse {
 
     private HttpStatus status;
